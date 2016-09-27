88e6ff575630342b082e2d17550007a9f2bb8b09
João Pinho <joao.pinho@tlantic.com>
2016-09-27 12:27:47 +0100

    update(taskItem) MRS-217 #comment Updated model #time 10m

--- tasks/models/task_setup.go
+++ tasks/models/task_setup.go
@@ -1,24 +1,48 @@
 package models
 
-type TaskSetupAttributes map[string]interface{}
+import "time"
 
-type TaskSetupName struct {
-	Value      string `json:"value"`
-	LanguageId string `json:"languageId"`
-}
 
 type TaskSetup struct {
-	Uid             string              `json:"_uId"`
-	SetupId         string              `json:"setupId,omitempty"`
-	OrganizationID  string              `json:"organizationId,omitempty"`
-	Status          string              `json:"status,omitempty"`
-	TaskType        string              `json:"taskType,omitempty"`
-	TaskName        []TaskSetupName     `json:"taskName,omitempty"`
-	TaskDescription string              `json:"taskDescription,omitempty"`
-	Periodicity     string              `json:"periodicity,omitempty"`
-	LastRunTime     int                 `json:"lastRunTime,omitempty"`
-	GroupIds        []string            `json:"groupIds,omitempty"`
-	StoreIds        []string            `json:"storeIds,omitempty"`
-	ApplicationIds  []string            `json:"applicationIds,omitempty"`
-	Attributes      TaskSetupAttributes `json:"attributes,omitempty"`
+
+	// Meta
+	Id 							string 					`json:"_uId" validate:"nonzero"`
+	Type 						string 					`json:"_type"`
+	CreateDate 					time.Time 				`json:"_createDate" validate:"nonzero"`
+	UpdateDate 					time.Time				`json:"_updateDate" validate:"nonzero"`
+	CreateUser 					string 					`json:"_createUser"`
+	UpdateUser 					string 					`json:"_updateUser"`
+	Status						string					`json:"status" validate:"nonzero, min=1, max=2"`
+	OrganizationId  			string              	`json:"organizationId" validate:"nonzero"`
+
+	// (cont.) Meta Data
+	SetupId             		string              	`json:"setupId" validate:"nonzero"`
+	StoreIds        			[]string            	`json:"storeIds"`
+	ApplicationIds  			[]string            	`json:"applicationIds"`
+	GroupIds  					[]string            	`json:"groupIds"`
+
+
+	// Task Descriptive
+	TaskType					string					`json:"taskType" validate:"nonzero"`
+
+	TaskName					string					`json:"name"`
+	TaskName_gb					string					`json:"name_gb,omitempty" validate:"nonzero"`
+	TaskName_pt					string					`json:"name_es,omitempty" validate:"nonzero"`
+	TaskName_es					string					`json:"name_es,omitempty" validate:"nonzero"`
+	TaskName_de					string					`json:"name_de,omitempty" validate:"nonzero"`
+	TaskName_us					string					`json:"name_us,omitempty" validate:"nonzero"`
+	TaskName_br					string					`json:"name_br,omitempty" validate:"nonzero"`
+	TaskName_ve					string					`json:"name_ve,omitempty" validate:"nonzero"`
+
+	TaskDescription				string					`json:"description"`
+	TaskDescription_gb			string					`json:"description_gb,omitempty" validate:"nonzero"`
+	TaskDescription_pt			string					`json:"description_es,omitempty" validate:"nonzero"`
+	TaskDescription_es			string					`json:"description_es,omitempty" validate:"nonzero"`
+	TaskDescription_de			string					`json:"description_de,omitempty" validate:"nonzero"`
+	TaskDescription_us			string					`json:"description_us,omitempty" validate:"nonzero"`
+	TaskDescription_br			string					`json:"description_br,omitempty" validate:"nonzero"`
+	TaskDescription_ve			string					`json:"description_ve,omitempty" validate:"nonzero"`
+
+	Periodicity    				time.Time           	`json:"periodicity"`
+	Attributes      			map[string]interface{} 	`json:"attributes"`
 }
